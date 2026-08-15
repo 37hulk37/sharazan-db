5ffd49a3cec19109d490d6287281ce5b7a053d0e
Alexander Ushakov <60547863+37hulk37@users.noreply.github.com>
2026-08-15 10:22:10 +0300

    code polishing

--- src/main/kotlin/com/sharazan/db/configuration/config.kt
+++ src/main/kotlin/com/sharazan/db/configuration/config.kt
@@ -4,30 +4,22 @@ import com.sharazan.core.AppBuilder
 import com.sharazan.core.Lifecycle
 import com.sharazan.core.properties.ConfigurationSource
 import com.sharazan.db.ExposedDatabase
+import org.koin.core.scope.Scope
 import org.koin.dsl.bind
 import org.koin.dsl.module
 
-fun AppBuilder.database(block: DbProperties.() -> Unit) = apply {
-    val props = DbProperties().apply(block)
-
-    val dbModule = module {
-        single { props } bind DbProperties::class
-        single { ExposedDatabase(props) } bind Lifecycle::class
-    }
-
-    addModule(dbModule)
+fun AppBuilder.database(block: DbProperties.() -> Unit) = registerDatabase {
+    DbProperties().apply(block)
 }
 
-fun AppBuilder.database() = apply {
+fun AppBuilder.database() = registerDatabase {
+    get<ConfigurationSource>()
+        .get<DbProperties>("sharazan.db")
+}
 
-
+private fun AppBuilder.registerDatabase(props: Scope.() -> DbProperties) = apply {
     val dbModule = module {
-        single {
-            val source = get<ConfigurationSource>()
-            val props = source.get<DbProperties>("sharazan.db")
-
-            props
-        }
+        single { props() } bind DbProperties::class
         single { ExposedDatabase(get()) } bind Lifecycle::class
     }
 
